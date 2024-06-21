5a6ee7ce3ce285a7994cb90425f6932472efe056
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2024-06-21 23:57:57 +0530

    Add files via upload

---
+++ js/typing.js
@@ -0,0 +1,14 @@
+var typing = new Typed("#typing", {
+  strings: [
+    " a front-end developer",
+    "a freelancer",
+    "a problem solver",
+    "a programmer",
+    "an aspiring software engineer",
+  ],
+  typeSpeed: 25,
+  backSpeed: 0,
+  fadeOut: true,
+  loop: true,
+  backDelay: 2000,
+});
